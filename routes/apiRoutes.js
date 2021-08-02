60f6cdbada6c201cab8cac6a55482c031796e4e5
Dana Bottoni <danabottoni@gmail.com>
2021-08-02 12:51:13 -0400

    htmlRoutes done

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+
+router.get('/api/notes', (req, res) => {
+  let results = notes;
+  if (req.query) {
+    
+  }
+
+
+})
+
+
+
+module.exports = router;
